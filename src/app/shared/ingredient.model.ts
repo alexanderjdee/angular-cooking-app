a61bbbf45c24dff2cfb942e85ffd8e03a36f09cd
adee <adee4990@gmail.com>
2018-12-12 22:11:56 -0500

    Project component setup

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+  //added accessors in front of properties, which automatically creates properties from parameters
+  constructor (public name: string, public amount: number){}
+}
