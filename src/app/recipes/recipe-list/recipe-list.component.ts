38dec0b02bdc6027da5a8523b6b61f16f1f68f06
alexander.dee <alexander.dee@bisimulations.com>
2018-12-14 13:29:01 -0500

    Additional databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test recipe', 'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
-    new Recipe('Test Recipe', 'This is a test recipe', 'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg')
+    new Recipe('Test Recipe', 'This is a test recipe', 'http://pureandapplied.com/preview/wp-content/uploads/2013/05/MyRecipes2.jpg'),
+    new Recipe('Another Test Recipe', 'This is another test recipe', 'http://pureandapplied.com/preview/wp-content/uploads/2013/05/MyRecipes2.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
